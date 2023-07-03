7e42693d259d4b32ed9b478f0cc5aba30b048e9a
yongjia <a592070@gmail.com>
2023-07-03 18:38:56 +0800

    [Server]embded react built file

--- main.go
+++ main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	"embed"
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"io/fs"
 	"net/http"
 )
 
-//go:embed public/*
-var app embed.FS
+var (
+	//go:embed client/build
+	react embed.FS
+
+	////go:embed public
+	//app embed.FS
+)
 
 func main() {
 	engine := gin.Default()
 
-	subFS, err := fs.Sub(app, "public")
-	if err != nil {
-		panic(err)
-	}
-	engine.StaticFS("/", http.FS(subFS))
+	engine.Use(EmbedReact("/", "client/build", react))
 
-	engine.Run(":3000")
+	engine.Run(":3001")
+}
+
+func EmbedReact(urlPrefix, buildDirectory string, em embed.FS) gin.HandlerFunc {
+	dir := static.LocalFile(buildDirectory, true)
+	embedDir, _ := fs.Sub(em, buildDirectory)
+	fileserver := http.FileServer(http.FS(embedDir))
+
+	if urlPrefix != "" {
+		fileserver = http.StripPrefix(urlPrefix, fileserver)
+	}
+
+	return func(c *gin.Context) {
+		if !dir.Exists(urlPrefix, c.Request.URL.Path) {
+			c.Request.URL.Path = "/"
+		}
+		fileserver.ServeHTTP(c.Writer, c.Request)
+		c.Abort()
+	}
 }
